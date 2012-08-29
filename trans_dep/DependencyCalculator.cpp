91bc972c9d8e140c165faaad766d42d89cb078f6
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2012-08-29 21:56:21 +0200

    david peti

--- trans_dep/DependencyCalculator.cpp
+++ trans_dep/DependencyCalculator.cpp
@@ -1,6 +1,7 @@
 #include "DependencyCalculator.hpp"
 #include <sstream>
 #include <iterator>
+#include <cassert>
 
 
 DependencyCalculator::DependencyCalculator( std::istream &in )
@@ -25,6 +26,26 @@ DependencyCalculator::doTheDependecyCalculationPlease(
     std::deque<NodeId>& beenThere,
     std::set<NodeId>& doneThat ) const
 {
+  if (beenThere.end() != std::find(beenThere.begin(), beenThere.end(), nodeId))
+  {
+    throw std::runtime_error("Circular dependency detected");
+  }
+  auto deps = m_graph.find(nodeId);
+  if (m_graph.end() == deps)
+  {
+    return {};
+  }
+  beenThere.push_front(nodeId);
+  std::vector<NodeId> returnDeps;
+  for ( const auto& dep : deps->second){
+    if (beenThere.end() == doneThat.find(nodeId))
+    {
+      returnDeps.append( doTheDependecyCalculationPlease( dep, beenThere, doneThat ));
+    }
+  }
+  assert(beenThere.front() == nodeId);
+  beenThere.pop_front();
+  doneThat.insert(nodeId);
   return {};
 }
 
